f003fe83dd97ab990b190bc460540661749efe5c
Braden Ganetsky <braden.ganetsky@gmail.com>
2025-10-01 20:17:50 -0500

    Write a json schema for the PartStacker project

--- src/pstack/gui/save.cpp
+++ src/pstack/gui/save.cpp
@@ -61,6 +61,127 @@ JSONCONS_ALL_MEMBER_TRAITS(pstack::gui::internal_save_state,
     pref, stack, sinterbox, parts, results
 );
 
+namespace {
+
+const std::string_view the_schema = R"the_schema({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "description": "A representation of the state of the PartStacker project",
+    "type": "object",
+    "$defs": {
+        "unsigned_int": {
+            "type": "integer",
+            "minimum": 0
+        },
+        "point3": {
+            "type": "object",
+            "properties": {
+                "x": { "type": "number" },
+                "y": { "type": "number" },
+                "z": { "type": "number" }
+            },
+            "required": ["x", "y", "z"]
+        },
+        "matrix3": {
+            "type": "object",
+            "properties": {
+                "xx": { "type": "number" },
+                "xy": { "type": "number" },
+                "xz": { "type": "number" },
+                "yx": { "type": "number" },
+                "yy": { "type": "number" },
+                "yz": { "type": "number" },
+                "zx": { "type": "number" },
+                "zy": { "type": "number" },
+                "zz": { "type": "number" }
+            },
+            "required": ["xx", "xy", "xz", "yx", "yy", "yz", "zx", "zy", "zz"]
+        }
+    },
+    "properties": {
+        "preferences": {
+            "type": "object",
+            "properties": {
+                "invert_scroll": { "type": "boolean" },
+                "extra_parts": { "type": "boolean" },
+                "show_bounding_box": { "type": "boolean" }
+            }
+        },
+        "stack": {
+            "type": "object",
+            "properties": {
+                "resolution": { "type": "number" },
+                "x_min": { "$ref": "#/$defs/unsigned_int" },
+                "x_max": { "$ref": "#/$defs/unsigned_int" },
+                "y_min": { "$ref": "#/$defs/unsigned_int" },
+                "y_max": { "$ref": "#/$defs/unsigned_int" },
+                "z_min": { "$ref": "#/$defs/unsigned_int" },
+                "z_max": { "$ref": "#/$defs/unsigned_int" }
+            }
+        },
+        "sinterbox": {
+            "type": "object",
+            "properties": {
+                "clearance": { "type": "number" },
+                "thickness": { "type": "number" },
+                "width": { "type": "number" },
+                "spacing": { "type": "number" }
+            }
+        },
+        "parts": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "mesh_file": { "type": "string" },
+                    "quantity": { "$ref": "#/$defs/unsigned_int" },
+                    "mirrored": { "type": "boolean" },
+                    "min_hole": { "$ref": "#/$defs/unsigned_int" },
+                    "rotation_index": { "type": "integer", "minimum": 0, "maximum": 2 },
+                    "rotate_min_box": { "type": "boolean" },
+                    "in_parts_list": { "type": "boolean" }
+                },
+                "required": ["mesh_file", "quantity", "mirrored", "min_hole", "rotation_index", "rotate_min_box", "in_parts_list"]
+            }
+        },
+        "results": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "pieces": {
+                        "type": "array",
+                        "items": {
+                            "type": "object",
+                            "properties": {
+                                "part": { "$ref": "#/$defs/unsigned_int" },
+                                "rotation": { "$ref": "#/$defs/matrix3" },
+                                "translation": { "$ref": "#/$defs/point3" }
+                            },
+                            "required": ["rotation", "translation", "part"]
+                        }
+                    },
+                    "sinterbox": {
+                        "type": "object",
+                        "properties": {
+                            "clearance": { "type": "number" },
+                            "thickness": { "type": "number" },
+                            "width": { "type": "number" },
+                            "spacing": { "type": "number" },
+                            "min": { "$ref": "#/$defs/point3" },
+                            "max": { "$ref": "#/$defs/point3" }
+                        },
+                        "required": ["clearance", "thickness", "width", "spacing", "min", "max"]
+                    }
+                },
+                "required": ["pieces"]
+            }
+        }
+    },
+    "required": ["preferences", "stack", "sinterbox", "parts", "results"]
+})the_schema";
+
+} // namespace
+
 namespace pstack::gui {
 
 namespace {
