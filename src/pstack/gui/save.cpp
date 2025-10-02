9e876f17de8c2981b60809adfb51b0392d4a2fc2
Braden Ganetsky <braden.ganetsky@gmail.com>
2025-10-01 22:45:18 -0500

    Write save_state_from_json()

--- src/pstack/gui/save.cpp
+++ src/pstack/gui/save.cpp
@@ -1,5 +1,6 @@
 #include "pstack/gui/save.hpp"
 #include <jsoncons/json.hpp>
+#include <jsoncons_ext/jsonschema/jsonschema.hpp>
 #include <cstddef>
 #include <expected>
 #include <string_view>
@@ -186,6 +187,40 @@ namespace pstack::gui {
 
 namespace {
 
+in_save_state from_internal(const internal_save_state& state) {
+	in_save_state s{};
+	s.pref = state.pref;
+	s.stack = state.stack;
+	s.sinterbox = state.sinterbox;
+
+	for (const save_part& part : state.parts) {
+		auto& list = part.in_parts_list ? s.parts : s.extra_parts;
+		calc::part p = calc::initialize_part(static_cast<const calc::part_base&>(part));
+		list.push_back(std::make_shared<calc::part>(std::move(p)));
+	}
+
+	for (const save_result& result : state.results) {
+		auto& r = s.results.emplace_back();
+		if (result.sinterbox.has_value()) {
+			r.sinterbox.emplace();
+			r.sinterbox->settings = static_cast<const calc::sinterbox_settings&>(*result.sinterbox);
+			r.sinterbox->bounding = static_cast<const calc::sinterbox_bounding&>(*result.sinterbox);
+		}
+		for (const auto& piece : result.pieces) {
+			auto& p = r.pieces.emplace_back();
+			p.rotation = piece.rotation;
+			p.translation = piece.translation;
+			if (piece.part < s.parts.size()) {
+				p.part = s.parts[piece.part];
+			} else {
+				p.part = s.extra_parts[piece.part - s.parts.size()];
+			}
+		}
+	}
+
+    return s;
+};
+
 internal_save_state to_internal(const out_save_state& state) {
     std::vector all_parts = state.parts; // Copy
 
@@ -230,7 +265,39 @@ internal_save_state to_internal(const out_save_state& state) {
 } // namespace
 
 std::expected<in_save_state, std::string> save_state_from_json(std::string_view str) {
-    return {};
+    try {
+
+        const jsoncons::json j = jsoncons::json::parse(str);
+
+        static const auto schema = []{
+            auto schema = jsoncons::json::parse(the_schema);
+            return jsoncons::jsonschema::make_json_schema(std::move(schema));
+        }();
+
+        std::vector<std::string> errors;
+        const auto schema_reporter = [&](const jsoncons::jsonschema::validation_message& message) {
+            errors.push_back(std::format("{}: {}", message.instance_location().string(), message.message()));
+            return jsoncons::jsonschema::walk_result::advance;
+        };
+        schema.validate(j, schema_reporter);
+        if (not errors.empty()) {
+            errors.insert(errors.begin(), "File does not conform to schema:");
+            auto view = errors | std::views::join_with(std::string_view("\n    "));
+            std::string full_error{view.begin(), view.end()};
+            return std::unexpected(std::move(full_error));
+        }
+
+        return from_internal(j.as<internal_save_state>());
+
+    } catch (const jsoncons::ser_error& e) {
+        return std::unexpected(std::string("Failed to parse JSON with ser_error: ") + e.what());
+    } catch (const jsoncons::json_exception& e) {
+        return std::unexpected(std::string("Failed to parse JSON with json_exception: ") + e.what());
+    } catch (const std::exception& e) {
+        return std::unexpected(std::string("Failed to parse JSON with std::exception: ") + e.what());
+    } catch (...) {
+        return std::unexpected("Failed to parse JSON with unknown error");
+    }
 }
 
 std::string save_state_to_json(const out_save_state& state) {
